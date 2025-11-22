71802ffefb9434c1b000d7714eb70c0007a38276
Cihat Köse <cihat.kose@hotmail.com>
2025-11-22 17:32:07 +0100

    Add a new Apache POI example

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -6,6 +6,7 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import utilities.DriverManager;
+import utilities.ExcelUtility;
 
 public class Hooks {
     @Before
@@ -15,6 +16,15 @@ public class Hooks {
 
     @After
     public void after(Scenario scenario) {
+
+        ExcelUtility.writeToExcel("src/test/java/apachePOI/ScenarioResults.xlsx",
+                scenario, DriverManager.getThreadBrowserName());
+
+        if (scenario.isFailed()) {
+            TakesScreenshot screenshot = ((TakesScreenshot) DriverManager.getDriver());
+            byte[] stateInMemory = screenshot.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(stateInMemory, "image/png", "screenshot name");
+        }
 
         System.out.println("The scenario finished.");
 
--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -4,9 +4,15 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.testng.Assert;
 import pages.Login;
 import utilities.DriverManager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class LoginSteps {
     Login login = new Login();
@@ -31,11 +37,26 @@ public class LoginSteps {
     @Then("Verify thar user is redirected to account summary")
     public void verifyTharUserIsRedirectedToAccountSummary() {
         String expectedUrl = "http://zero.webappsecurity.com/index.html";
-        Assert.assertEquals(DriverManager.getDriver().getCurrentUrl(), expectedUrl,"User was not redirected to account summary page.");
+        Assert.assertEquals(DriverManager.getDriver().getCurrentUrl(), expectedUrl, "User was not redirected to account summary page.");
     }
 
     @And("Verify that user icon is displayed")
     public void verifyThatUserIconIsDisplayed() {
-        Assert.assertTrue(login.userIcon.isDisplayed(),"User icon is not displayed");
+        Assert.assertTrue(login.userIcon.isDisplayed(), "User icon is not displayed");
+    }
+
+    @And("Retrieve and enter username and password from Excel file {string}")
+    public void retrieveAndEnterUsernameAndPasswordFromExcelFile(String fileName) throws IOException {
+        String path = "src/test/java/apachePOI/" + fileName;
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Sheet sheet = workbook.getSheet("Sheet1");
+
+        String username = String.valueOf(sheet.getRow(4).getCell(5));
+        String password = String.valueOf(sheet.getRow(4).getCell(6));
+
+        login.mySendKeys(login.username, username);
+        login.mySendKeys(login.password, password);
     }
 }
